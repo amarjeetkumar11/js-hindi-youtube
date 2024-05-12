2879ffb54776fb4bc82fbb547497e1c7f8b759cf
Amarjeet kumar <52212124+amarjeetkumar11@users.noreply.github.com>
2024-05-12 17:58:48 +0000

    variable basics

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const account_Id = 1267;
+let acc_email= 'abd';
+var accPass='122';
+accPass=12;
+// account_Id=12; not allowed
+//let is used in common practice
+
+
+
+console.log(accPass);
+console.table([accPass,acc_email,account_Id])
